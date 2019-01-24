3c8c7b6204693c517f620919619d22d841386233
Dang Duy Cuong <duycuongkma@gmail.com>
2019-01-24 15:44:39 +0700

    SlideMenu Simple Lam theo

---
+++ SlideMenuDemo0/SlideMenuDemo0Tests/SlideMenuDemo0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SlideMenuDemo0Tests.swift
+//  SlideMenuDemo0Tests
+//
+//  Created by duycuong on 1/23/19.
+//  Copyright © 2019 duycuong. All rights reserved.
+//
+
+import XCTest
+@testable import SlideMenuDemo0
+
+class SlideMenuDemo0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
